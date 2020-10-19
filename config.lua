d3548dfcb681cf66756156055b0c1b4bc789d3b0
Jordan.#2139 <jordansilva213@gmail.com>
2020-10-19 12:26:59 -0400

    updated ped density

--- config.lua
+++ config.lua
@@ -6,11 +6,11 @@
 --           CONFIG YOUR PERIPHERALS HERE!           --
 -------------------------------------------------------
 Config = {
-    VehDensity = 1, -- How many vehicles are driving (10 - 1) | The lower the number the less cars
-    PedDensity = 1, -- How many AI are around (10 - 1) | The lower the number the less AI
-    RanVehDensity = 1, -- How many Random Cars (10 - 1) | The lower the number the less cars
-    ParkCarDensity = 1, -- How many parked cars (10 - 1) | The lower the number the less cars
-    ScenePedDensity = 1, -- How many AI doing emotes (scenarios) (10 - 1) | The lower the number the less AI
+    VehDensity = 0.85, -- How many vehicles are driving (10 - 1) | The lower the number the less cars
+    PedDensity = 0.75, -- How many AI are around (10 - 1) | The lower the number the less AI
+    RanVehDensity = 0.65, -- How many Random Cars (10 - 1) | The lower the number the less cars
+    ParkCarDensity = 0.95, -- How many parked cars (10 - 1) | The lower the number the less cars
+    ScenePedDensity = 0.35, -- How many AI doing emotes (scenarios) (10 - 1) | The lower the number the less AI
     }
     --------------------------
     -- ^^^ DO THAT HERE ^^^ -- 
