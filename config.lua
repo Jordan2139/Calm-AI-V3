dd30ac9732f3385748b6f308170060d8d7817eb8
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2020-10-20 08:12:28 -0400

    Can't type

--- config.lua
+++ config.lua
@@ -6,11 +6,11 @@
 --           CONFIG YOUR PERIPHERALS HERE!           --
 -------------------------------------------------------
 Config = {
-    VehDensity = 0.85, -- How many vehicles are driving (10 - 1) | The lower the number the less cars
-    PedDensity = 0.75, -- How many AI are around (10 - 1) | The lower the number the less AI
-    RanVehDensity = 0.65, -- How many Random Cars (10 - 1) | The lower the number the less cars
-    ParkCarDensity = 0.95, -- How many parked cars (10 - 1) | The lower the number the less cars
-    ScenePedDensity = 0.35, -- How many AI doing emotes (scenarios) (10 - 1) | The lower the number the less AI
+    VehDensity = 0.85, -- How many vehicles are driving (0.0 - 1.0) | The lower the number the less cars
+    PedDensity = 0.75, -- How many AI are around (0.0 - 1.0) | The lower the number the less AI
+    RanVehDensity = 0.65, -- How many Random Cars (0.0 - 1.0) | The lower the number the less cars
+    ParkCarDensity = 0.95, -- How many parked cars (0.0 - 1.0) | The lower the number the less cars
+    ScenePedDensity = 0.35, -- How many AI doing emotes (scenarios) (0.0 - 1.0) | The lower the number the less AI
     }
     --------------------------
     -- ^^^ DO THAT HERE ^^^ -- 
